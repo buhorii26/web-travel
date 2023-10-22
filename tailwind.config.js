5f7a9ac00277a81abc97675e6eb141b2275bcfa6
Buhori <buhorics@gmail.com>
2023-10-22 13:22:29 +0900

    update website

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,24 @@ module.exports = {
         "pink" : '#F85E9F',
         "orange" : '#FF5722',
         "grey" : '#222831',
+      },
+      dropShadow: {
+        "sm-shadow": [
+          '0 1px 2px rgb(0 0 0 / 0.1)',
+          '0 1px 1px rgb(0 0 0 / 0.05)',
+          '0 2px 2px rgb(0 0 0 / 0.06)',
+          '0 10px 8px rgb(0 0 0 / 0.04)',
+          '0 4px 3px rgb(0 0 0 / 0.1)',
+          '0 20px 13px rgb(0 0 0 / 0.03)',
+        ],
+        "lg-shadow":[
+          '0 1px 2px rgb(0 0 0 / 0.1)',
+          '0 1px 1px rgb(0 0 0 / 0.05)',
+          '0 2px 2px rgb(0 0 0 / 0.06)',
+          '0 10px 8px rgb(0 0 0 / 0.04)',
+          '0 4px 3px rgb(0 0 0 / 0.1)',
+          '0 20px 13px rgb(0 0 0 / 0.03)',
+        ],
       }
     },
   },
